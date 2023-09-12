a2b85b829284d5629f368948ee4a4bac64486d6e
JoseGP39 <144749559+JoseGP39@users.noreply.github.com>
2023-09-11 20:11:01 -0500

    Update logica.js
    Siclo SWITCH en javaScript

--- logica.js
+++ logica.js
@@ -1,1 +1,40 @@
-
+var themeNumber = parseInt(readLine(themeNumber),
+    10);
+    /*
+    1 - Light
+    2 - Dark
+    3 - Nocturne
+    4 - Terminal
+    5 - Indigo
+    */
+    // Tu código va aquí
+    
+    
+    switch (themeNumber){
+    
+    case 1:
+    console.log("Light");
+    break;
+    
+    
+    case 2:
+    console.log("Dark");
+    break;
+    
+    case 3:
+    console.log("Noctune");
+    break;
+    
+    case 4:
+    console.log("Terminal");
+    break;
+    
+    case 5:
+    console.log("Indigo");
+    break;
+    
+    default:
+    console.log("Another color");
+    
+    }
+}
